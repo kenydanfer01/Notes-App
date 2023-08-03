6321cad778c6e30c64fad4aa68a4f5de75263753
kenydanfer01 <kenychumacero19@gmail.com>
2023-08-03 13:40:15 -0500

    [backend] define initial data

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO notes (title, content, is_active) VALUES ('Note 1', 'Content for Note 1', 1);
+INSERT INTO notes (title, content, is_active) VALUES ('Note 2', 'Content for Note 2', 1);
+INSERT INTO notes (title, content, is_active) VALUES ('Note 3', 'Content for Note 3', 1);
+INSERT INTO notes (title, content, is_active) VALUES ('Note 4', 'Content for Note 4', 0);
+INSERT INTO notes (title, content, is_active) VALUES ('Note 5', 'Content for Note 5', 1);
+INSERT INTO notes (title, content, is_active) VALUES ('Note 6', 'Content for Note 6', 0);
+INSERT INTO notes (title, content, is_active) VALUES ('Note 7', 'Content for Note 7', 1);
+INSERT INTO notes (title, content, is_active) VALUES ('Note 8', 'Content for Note 8', 1);
+INSERT INTO notes (title, content, is_active) VALUES ('Note 9', 'Content for Note 9', 0);
+INSERT INTO notes (title, content, is_active) VALUES ('Note 10', 'Content for Note 10', 1);
